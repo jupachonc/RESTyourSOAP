597d4c10bbb91f7d174ce242f7ee213be106b1f1
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2023-05-16 19:31:22 -0500

    Initial commit

---
+++ src/main/java/co/restursoap/restyoursoap/ResTyourSoapApplication.java
@@ -0,0 +1,13 @@
+package co.restursoap.restyoursoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTyourSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTyourSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/restursoap/restyoursoap/ResTyourSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package co.restursoap.restyoursoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTyourSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
